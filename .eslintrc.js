b46d17b06646f38e1edf3f9dae5632f8b8900d4c
HITAND <polishball.pl12@gmail.com>
2023-04-27 18:19:52 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+﻿module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  overrides: [],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    "arrow-body-style": 1,
+    "prefer-arrow-callback": 1,
+    "react/prop-types": 0,
+    "no-unused-vars": 1,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
